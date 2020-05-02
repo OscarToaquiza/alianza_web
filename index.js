04d16489f729eaa19314bb6029e2e96bd04ab473
OscarToaquiza <oscartoaquiza388@gmail.com>
2020-05-02 16:26:11 +0200

    Cambio de puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 4444;
+var port = process.env.PORT || 3000;
 
 var uri = 'mongodb://alianza:2DlgfnC3oz4Qo9rL@cluster0-shard-00-00-hvjja.mongodb.net:27017,cluster0-shard-00-01-hvjja.mongodb.net:27017,cluster0-shard-00-02-hvjja.mongodb.net:27017/aee?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 //var uri = 'mongodb://localhost:27017/aee';
